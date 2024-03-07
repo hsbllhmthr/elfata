0b3b04ba5863a4ca660664399f7724e95a73c8b0
bulls <bullahdedat0@gmail.com>
2024-03-07 16:16:41 +0800

    new app version

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -3,7 +3,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 p-4 d-flex align-items-center text-wrap" href="{{ route('dasbor') }}">
             <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-2 font-weight-bold text-white">El-Fata<br />Foundation</span>
+            <span class="ms-2 font-weight-bold text-white">Elfata<br />Indonesia</span>
         </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
